4162f065e5f0d861e41af04b42eca98a3c3656d4
anng-y <anngyip59@gmail.com>
2024-04-27 17:56:04 -0700

    added structures

---
+++ MooMenu/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  MooMenu
+//
+//  Created by Ann Yip on 4/27/24.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        Text("ProfileView")
+    }
+}
+
+#Preview {
+    ProfileView()
+}
